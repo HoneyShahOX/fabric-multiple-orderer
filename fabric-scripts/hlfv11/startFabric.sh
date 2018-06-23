2bea1137ddd71c0755bfaedcb36b820a17805d5a
Honey Shah <honey.shah@openxcellinc.com>
2018-06-23 09:35:13 +0530

    minor changes

--- fabric-scripts/hlfv11/startFabric.sh
+++ fabric-scripts/hlfv11/startFabric.sh
@@ -68,7 +68,7 @@ echo "sleeping for ${FABRIC_START_TIMEOUT} seconds to wait for fabric to complet
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
-docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c composerchannel -f /etc/hyperledger/configtx/composer-channel.tx
+docker exec peer0.org1.example.com peer channel create -o orderer1.example.com:7050 -c composerchannel -f /etc/hyperledger/configtx/composer-channel.tx
 
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b composerchannel.block
